b19c32c2fe8b64c8686ea6b1e731b80f8ecd917a
eunjung <enjung915@naver.com>
2024-07-29 22:45:07 +0900

    map-단어 개수
    STL map 자료구조 활용 - 단어 개수 카운트

--- map.cpp
+++ map.cpp
@@ -7,17 +7,24 @@ int main() {
 	ios_base::sync_with_stdio(false);
 	//ifstream cin;
 	//cin.open("input.txt"); 
-	map<char,int> ch;
-	map<char,int>::iterator it;
+	map<string,int> ch;
+	map<string,int>::iterator it;
 	char a[100];
-	cin>>a;
+    int n;
+	cin>>n;
+    for(int i=1;i<=n;i++){
+        cin>>a;
+        ch[a]++;
+    }
 	
-	for(int i=0;a[i]!='\0';i++){
-		ch[a[i]]++;
-	}
-	
+    int max=0;
+    string res;
 	for(it=ch.begin();it!=ch.end();it++){
-		cout<< it->first << ' '<< it->second <<"\n";
+        if(it->second >max){
+            max = it->second;
+            res = it->first;
+        }
 	}
+    cout<<res<<" : "<<max<<"\n";
 	return 0;
 }
